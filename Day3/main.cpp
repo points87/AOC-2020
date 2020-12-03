b6a1730a9a61e34e2df86229a681304e3bc0f843
Points 87 <46542922+points87@users.noreply.github.com>
2020-12-03 19:20:34 +0100

    Format the code

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -8,11 +8,11 @@ int is_integer(double num) {
 //m : slope, x = m * y
 int solutionOne(std::vector<std::string> &data, double m) {
     int treeCount = 0;
-    for(int y =0; y < data.size(); y++) {
+    for (int y = 0; y < data.size(); y++) {
         std::string row = data.at(y);
         double x = std::fmod(m * y, row.length());
-        if( is_integer(x) && row[x] == '#') {
-            treeCount ++;
+        if (is_integer(x) && row[x] == '#') {
+            treeCount++;
         }
     }
     return treeCount;
@@ -21,7 +21,7 @@ int solutionOne(std::vector<std::string> &data, double m) {
 unsigned int solutionTwo(std::vector<std::string> &data) {
     unsigned int treeCount = 1;
     std::vector<double> slopes = {1, 3, 5, 7, 0.5};
-    for(double slope : slopes) {
+    for (double slope : slopes) {
         treeCount = treeCount * solutionOne(data, slope);
     }
     return treeCount;
@@ -31,9 +31,9 @@ unsigned int solutionTwo(std::vector<std::string> &data) {
 int main() {
     std::vector<std::string> data;
 
-    if(input(data)) {
-        std::cout<<solutionOne(data, 3)<<std::endl;
-        std::cout<<solutionTwo(data) << std::endl;
+    if (input(data)) {
+        std::cout << solutionOne(data, 3) << std::endl;
+        std::cout << solutionTwo(data) << std::endl;
     }
     return 0;
 }
